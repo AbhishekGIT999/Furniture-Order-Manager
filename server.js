f62078371db151020ada4572a35a31f102d09e17
RENU DEVI <renudevi689111@gmail.com>
2026-07-15 12:36:35 +0530

    Configure Express to serve static files from public folder

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
+app.use(express.static("public"));
 
 app.use(cors());
 app.use(bodyParser.json());
